f8fdfad56668e34da26a8cd0b3038d6257506739
arthurgomespassos <arthurgomespassos74@gmail.com>
2022-03-22 22:50:16 -0300

    setup incial configurado para projeto

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@babel/eslint-parser', // added by me
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/state-in-constructor': 'off',
+    'react/forbid-prop-types': 'off',
+  },
+};
